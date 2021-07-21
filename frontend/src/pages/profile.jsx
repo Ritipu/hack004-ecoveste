fd8203a9b67d85a7a9de8a3d24abbf11cabc2abf
danidash182 <70019275+danidash182@users.noreply.github.com>
2021-07-21 22:38:12 +0100

    chore: profile css beggining
    chore: profile css beggining

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -10,16 +10,33 @@ export default class Profile extends React.Component {
         this.state = {
             isLogged: false,
             isOpen: false,
+            
         }
     }
 
+    handleSubmit(event) {
+        console.log('Foi submetido um nome: ');
+        event.preventDefault();
+      }
+
+    // handleSubmit(event) {
+    //     event.preventDefault()
+    //     console.log("wow")
+    //     // fetch("/adddonor", {
+    //     //     method: "POST",
+    //     //     body: {},
+    //     //     headers: {
+    //     //         "Content-Type": "application/json"
+    //     //     }
+    //     // })
+    // }
 
     togglePopup() { this.setState((state) => ({ isOpen: !(state.isOpen) })) }
 
     render() {
-
         return (
-            <>
+            <div className="body">
+                <img className="logo" src="/assets/logo.png"></img>
                 <form className="form">
                     <label>
                         <input type="email" name="email" placeholder="Insira o seu email" />
@@ -33,24 +50,29 @@ export default class Profile extends React.Component {
 
                 {this.state.isOpen && <Popup
                     content={<>
-                        <div >
-                            <form>
+                        <div className="popup-content">
+                            <form onSubmit={() => this.handleSubmit()}>
+
                                 <label>
-                                    <input type="text" name="username" placeholder="Insira um username" />
+                                    <input type="text" name="username" placeholder="Insira o seu nome"  /><br />
                                 </label>
                                 <label>
-                                    <input type="email" name="email" placeholder="Insira o seu email" />
+                                    <input type="number" name="telefone" placeholder="Insira o seu número de telefone"  /><br />
                                 </label>
                                 <label>
-                                    <input type="password" name="password" placeholder="Insira a sua password" />
+                                    <input type="email" name="email" placeholder="Insira o seu email"  /><br />
                                 </label>
-                                <input type="submit" value="Submeter"></input>
+                                <label>
+                                    <input type="password" name="password" placeholder="Insira a sua password"  /><br />
+                                </label>
+
+                                <input type="submit" value="Registar" />
                             </form>
                         </div>
                     </>}
                     handleClose={() => this.togglePopup()}
                 />}
-            </>
+            </div>
 
         )
     }
