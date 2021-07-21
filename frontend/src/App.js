ca9c71564942a18d9c911cd509bb3a9b45548c30
Ritipu <82657453+Ritipu@users.noreply.github.com>
2021-07-21 12:04:51 +0100

    chore: update menu
    chore: update menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './css/App.css';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="header">
-          <img src="/assets/logo.png" className="logo"/>
+          <img src="/assets/logo.png" className="logo" />
         </div>
         <div>
           <h3>Searchbar</h3>
         </div>
         <div className="menu">
-          <p>Menu</p>
+          <button className="menu-btn">
+            <HomeIcon />
+            <span>Home</span>
+          </button>
+          <button className="menu-btn">
+            <AccountCircleIcon />
+            <span>Profile</span>
+          </button>
         </div>
       </div>
     );
