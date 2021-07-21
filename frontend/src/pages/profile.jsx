01a9e1fa1eb87937dd2c257ef4a8b9939c2e4c0c
josianelima <josianelima_@hotmail.com>
2021-07-21 18:21:58 +0100

    chore: Profile

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -1,11 +1,57 @@
+
 import React from 'react';
+import "../css/profile.css"
+import Popup from '../components/popup'
+
 
 export default class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLogged: false,
+            isOpen: false,
+        }
+    }
+
+
+    togglePopup() { this.setState((state) => ({ isOpen: !(state.isOpen) })) }
+
     render() {
+
         return (
             <>
-                <h1>Ola do Profile</h1>
+                <form className="form">
+                    <label>
+                        <input type="email" name="email" placeholder="Insira o seu email" />
+                    </label>
+                    <label>
+                        <input type="password" name="password" placeholder="Insira a sua password" />
+                    </label>
+                    <input type="submit" value="Submeter"></input>
+                </form>
+                <button onClick={() => this.togglePopup()} className="registar">Registe-se aqui</button>
+
+                {this.state.isOpen && <Popup
+                    content={<>
+                        <div >
+                            <form>
+                                <label>
+                                    <input type="text" name="username" placeholder="Insira um username" />
+                                </label>
+                                <label>
+                                    <input type="email" name="email" placeholder="Insira o seu email" />
+                                </label>
+                                <label>
+                                    <input type="password" name="password" placeholder="Insira a sua password" />
+                                </label>
+                                <input type="submit" value="Submeter"></input>
+                            </form>
+                        </div>
+                    </>}
+                    handleClose={() => this.togglePopup()}
+                />}
             </>
+
         )
     }
 }
