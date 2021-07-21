1a12608dc8cb73e09f71905406d5082371fd8bb1
Ritipu <82657453+Ritipu@users.noreply.github.com>
2021-07-21 18:09:49 +0100

    chore: search working 70%
    chore: search working 70%

--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from '../components/card'
 import '../css/search.css';
 
 export default class Search extends React.Component {
@@ -11,13 +12,34 @@ export default class Search extends React.Component {
         }
     }
 
-    componentDidMount() {
-        
+    async componentDidMount() {
+        await this.getCategoryInfo();
+    }
+
+    async getCategoryInfo() {
+        const res = await fetch(`/bycategory/${this.state.searchResult}`)
+        const json = await res.json()
+        const idFilter = json.product.map((e) => e._id)
+
+        this.setState({
+            results: idFilter,
+            numOfResults: idFilter.length
+        })
+        console.log(this.state.results)
     }
 
     render() {
         return (
-            <h3>Foram encontrados X artigos de {this.state.searchResult}</h3>
+            <div>
+                <h3>Foram encontrados {this.state.numOfResults} artigos de {this.state.searchResult}</h3>
+                {
+                    this.state.results.map(d => (
+                        <Card key={d}
+                        id={d}
+                        />
+                    ))
+                }
+            </div>
         )
     }
 }
