4a8241aa52eb46fadd9baba5034786a4f3aff891
Ritipu <82657453+Ritipu@users.noreply.github.com>
2021-07-22 12:29:53 +0100

    update: profile
    update: profile

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -5,76 +5,105 @@ import Popup from '../components/popup'
 
 
 export default class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLogged: false,
-            isOpen: false,
-            
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLogged: false,
+			isOpen: false,
+			name: '',
+			pw: '',
+			email: '',
+			numTelephone: '',
+		}
 
-    //dúvidas sobre formulários e event
-    handleSubmit(event) {
-        console.log('Foi submetido um nome: ');
-        event.preventDefault();
-      }
+		this.onSubmit = this.onSubmit.bind(this)
+		this.handlePassword = this.handlePassword.bind(this)
+	}
 
-    // handleSubmit(event) {
-    //     event.preventDefault()
-    //     console.log("wow")
-    //     // fetch("/adddonor", {
-    //     //     method: "POST",
-    //     //     body: {},
-    //     //     headers: {
-    //     //         "Content-Type": "application/json"
-    //     //     }
-    //     // })
-    // }
+	onSubmit() {
+		console.log('Submetido')
+		console.log('Password: ' + this.state.pw)
+		this.setState({ isOpen: false });
+	}
 
-    togglePopup() { this.setState((state) => ({ isOpen: !(state.isOpen) })) }
+	handleEmail(form) {
+		this.setState({ email: form.target.value })
+	}
 
-    render() {
-        return (
-            <div className="body">
-                <img className="logo" src="/assets/logo.png"></img>
-                <form className="form">
-                    <label>
-                        <input className="login-data" type="email" name="email" placeholder="Insira o seu email" required/>
-                    </label>
-                    <label>
-                        <input className="login-data" type="password" name="password" placeholder="Insira a sua password" required/>
-                    </label>
-                    <button className="submit" type="submit">Submeter</button>
-                </form>
-                <button onClick={() => this.togglePopup()} className="register">Registe-se aqui</button>
+	handlePassword(form) {
+		this.setState({ pw: form.target.value })
+	}
 
-                {this.state.isOpen && <Popup
-                    content={<>
-                        <div className="popup-content">
-                            <form onSubmit={() => this.handleSubmit()}>
+	// handleSubmit(event) {
+	//     event.preventDefault()
+	//     console.log("wow")
+	//     // fetch("/adddonor", {
+	//     //     method: "POST",
+	//     //     body: {},
+	//     //     headers: {
+	//     //         "Content-Type": "application/json"
+	//     //     }
+	//     // })
+	// }
 
-                                <label>
-                                    <input className="boxes" type="text" name="username" placeholder="Insira o seu nome" /><br />
-                                </label>
-                                <label>
-                                    <input className="boxes" type="text" name="telefone" placeholder="Insira o seu número de telefone" /><br />
-                                </label>
-                                <label>
-                                    <input className="boxes" type="email" name="email" placeholder="Insira o seu email"  /><br/>
-                                </label>
-                                <label>
-                                    <input className="boxes" type="password" name="password" placeholder="Insira a sua password"  /><br />
-                                </label>
+	togglePopup() { this.setState((state) => ({ isOpen: !(state.isOpen) })) }
 
-                                <button className="submitpop" type="submit">Submeter</button>
-                            </form>
-                        </div>
-                    </>}
-                    handleClose={() => this.togglePopup()}
-                />}
-            </div>
+	render() {
+		return (
+			<div className="body">
+				<img className="logo" src="/assets/logo.png"></img>
+				<form className="form">
+					<label>
+						<input className="login-data" type="email" name="email" placeholder="Insira o seu email" required />
+					</label>
+					<label>
+						<input className="login-data" type="password" name="password" placeholder="Insira a sua password" required />
+					</label>
+					<button className="submit" type="submit">Submeter</button>
+				</form>
+				<button onClick={() => this.togglePopup()} className="registar">Registe-se aqui</button>
 
-        )
-    }
+				{this.state.isOpen && <Popup
+					content={
+						<>
+							<div className="popup-content">
+								<form onSubmit={this.onSubmit}>
+
+									<label>
+										<input className="boxes" type="text" name="username" placeholder="Insira o seu nome" /><br />
+									</label>
+									<label>
+										<input className="boxes" type="text" name="telefone" placeholder="Insira o seu número de telefone" /><br />
+									</label>
+									<label>
+										<input 
+										className="boxes" 
+										type="email" 
+										name="email" 
+										placeholder="Insira o seu email"
+										value={this.state.email}
+										onChange={this.handleEmail}
+										/><br />
+									</label>
+									<label>
+										<input
+											className="boxes"
+											type="password"
+											name="password"
+											placeholder="Insira a sua password"
+											value={this.state.pw}
+											onChange={this.handlePassword}
+										/><br />
+									</label>
+
+									<button className="submitpop" type="submit">Submeter</button>
+								</form>
+							</div>
+						</>}
+					handleClose={() => this.togglePopup()}
+				/>}
+			</div>
+
+		)
+	}
 }
